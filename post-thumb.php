e269aec50e407c95844204372a845ecdd9e893b9
Dave Fischoff <davefischoff@gmail.com>
2014-10-04 13:06:21 -0400

    modified post-thumb.php

--- post-thumb.php
+++ post-thumb.php
@@ -1,11 +1,11 @@
 <?php include('tax-terms.php')  // Adds taxonomy class names to the post, via the $post_terms variable ?>
 <article id="post-<?php the_ID(); ?>" class="thumb <?php echo $post_terms ?> col-xs-4">	
-	<div class="flyer"><?php echo get_the_post_thumbnail($page->ID, 'large'); ?></div>
+	<div class="flyer"><?php echo get_the_post_thumbnail($page->ID, 'category-thumb'); ?></div>
 	
 	<div class="entry-info-wrap"><div class="entry-info">
 		<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'sscontent' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 
-		<div class="entry-desc">
+		<div class="entry-desc event-desc">
 			<?php the_excerpt(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:'), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-desc -->
