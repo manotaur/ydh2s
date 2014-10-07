0bc5eddf8266727df2f254f1d3b91c30f2b5a826
manotaur <cellophanesky@gmail.com>
2014-10-07 15:40:28 -0400

    Cleaning main.js up a bit

---
+++ main.js
@@ -0,0 +1,35 @@
+var startY = $('.header').offset.top + $('.header').outerHeight();
+var startY = 158;
+
+$(window).scroll(function(){
+	checkY();
+});
+
+function checkY(){
+	if( $(window).scrollTop() > startY ){
+		$('.sticky').show();
+	}	else{
+		$('.sticky').slideUp();
+	}
+};
+			
+$( function isoFilter(){
+	// Isotope setup
+	var $container = $('.filter-us').isotope({
+		itemSelector: '.thumb',
+		layoutMode: 'fitRows'
+	});
+	// On click, filter whatever is in the data-filter field
+	$('#filter').on( 'click', 'button', function() {
+		var filterValue = $( this ).attr('data-filter');
+		$container.isotope({ filter: filterValue });
+	});
+});
+
+$(document).ready(function() {
+	$('.btn-group button').click( function() { // Selector Button on/off script
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+	checkY();
+	isoFilter();
+});
